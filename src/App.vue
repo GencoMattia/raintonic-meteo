31cf8253857d473b3e269e61631c251319fee18b
GencoMattia <gencomattia@hotmail.it>
2024-10-29 13:57:14 +0100

    addes lang scss and styles link to style section in App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss">
+@use "../node_modules/bootstrap/scss/bootstrap.scss";
+@use "./assets/styles/general.scss";
+@use "./assets/styles/partials/variables" as *;
 
 </style>
