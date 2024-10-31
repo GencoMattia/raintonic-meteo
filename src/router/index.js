eab5c8855cb2a981be5c4e997da5317a606f3600
GencoMattia <gencomattia@hotmail.it>
2024-10-31 16:45:13 +0100

    Set up deploy on GitHub pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,20 @@
-import PageHome from '@/pages/PageHome.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from '@/pages/PageHome.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: PageHome
+  },
+  {
+
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: PageHome
-    },
-    {
-      
-    }
-  ]
+  routes,
 })
 
 export default router
